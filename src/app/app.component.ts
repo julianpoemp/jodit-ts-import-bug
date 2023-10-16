8b3446368005b1e0dab8708e812eeee0787a2bca
Julian Pömp <jp@poemp.net>
2023-10-16 11:12:55 +0200

    initialized project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as joditESM from 'jodit';
+import {Config} from 'jodit/esm/config';
+
+declare const Jodit: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'jodit-ts-bugs';
+export class AppComponent implements OnInit {
+  ngOnInit(){
+    joditESM.Jodit.make('#editor', {
+      width: 600,
+      height: 400,
+      buttons: ["bold", "underline"],
+      textIcons: false
+    } as Config);
+
+    Jodit.make('#editor2', {
+      width: 600,
+      height: 400,
+      buttons: ["bold", "underline"],
+      textIcons: false
+    } as Config);
+  }
 }
